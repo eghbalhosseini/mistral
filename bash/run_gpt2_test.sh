ec37a03b577026f652ea9ba0d2c8ff4188371fb9
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-02-03 18:04:09 -0500

    updated miniBERTa to read locally instead of huggingface

--- bash/run_gpt2_test.sh
+++ bash/run_gpt2_test.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=mini
 #SBATCH --array=0
 #SBATCH --time=2-12:00:00
-#SBATCH --gres=gpu:RTXA6000:3
+#SBATCH --gres=gpu:RTXA6000:4
 #SBATCH --ntasks=1
 #SBATCH --mem=120G
 #SBATCH --mail-type=ALL
@@ -15,4 +15,4 @@ conda activate mistral
 echo $(which python)
 cd /om/user/ehoseini/mistral/
 
-python -m torch.distributed.launch --nproc_per_node=4 --nnodes=1 --node_rank=0 train.py --config conf/OM_miniBERTa-1M-gpt2-micro.yaml --training_arguments.fp16 true --training_arguments.per_device_train_batch_size 64 --run_id minberta_1m
+python -m torch.distributed.launch --nproc_per_node=4 --nnodes=1 --node_rank=0 train.py --config conf/tutorial-gpt2-micro.yaml--training_arguments.fp16 true --training_arguments.per_device_train_batch_size 64 --run_id minberta_1m
