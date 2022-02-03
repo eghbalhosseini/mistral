7e62be1ac4526111ba83e88f9f52913a2e21d82a
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-02-03 18:06:29 -0500

    bugfix

--- bash/run_gpt2_test.sh
+++ bash/run_gpt2_test.sh
@@ -15,4 +15,4 @@ conda activate mistral
 echo $(which python)
 cd /om/user/ehoseini/mistral/
 
-python -m torch.distributed.launch --nproc_per_node=4 --nnodes=1 --node_rank=0 train.py --config conf/tutorial-gpt2-micro.yaml--training_arguments.fp16 true --training_arguments.per_device_train_batch_size 64 --run_id minberta_1m
+python -m torch.distributed.launch --nproc_per_node=4 --nnodes=1 --node_rank=0 train.py --config conf/tutorial-gpt2-micro.yaml --training_arguments.fp16 true --training_arguments.per_device_train_batch_size 64 --run_id minberta_1m
