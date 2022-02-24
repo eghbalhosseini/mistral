546aaf1e312e989c9de50e027b5aa4e6f060c455
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-02-24 15:58:11 -0500

    bugfix

--- bash/run_deepspeed_test.sh
+++ bash/run_deepspeed_test.sh
@@ -15,5 +15,4 @@ conda activate mistral
 echo $(which python)
 cd /om/user/ehoseini/mistral/
 
-deepspeed --num_gpus 4 --num_nodes 1 --master_addr node094 train.py --config conf/tutorial-gpt2-micro.yaml --nnodes 1 --nproc_per_node 4 --training_arguments.fp16 true --training_arguments.per_device_train_batch_size 2 --training_arguments.deepspeed conf/deepspeed/z3-conf.json \
- --run_id tutorial-gpt2-micro-multi-node > tutorial-gpt2-micro-multi-node.out 2 tutorial-gpt2-micro-multi-node.err --hostfile conf/deepspeed/hostfile_node094
+deepspeed --num_gpus 4 --num_nodes 1 --master_addr node094 train.py --config conf/tutorial-gpt2-micro.yaml --nnodes 1 --nproc_per_node 4 --training_arguments.fp16 true --training_arguments.per_device_train_batch_size 2 --training_arguments.deepspeed conf/deepspeed/z3-conf.json --run_id tutorial-gpt2-micro-multi-node > tutorial-gpt2-micro-multi-node.out 2> tutorial-gpt2-micro-multi-node.err --hostfile conf/deepspeed/hostfile_node094
