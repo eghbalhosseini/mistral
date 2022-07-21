7921b720b67e0f7b9df4dcef43a50096ef3baa20
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2022-07-21 15:09:49 -0400

    bugfix

--- bash/run_mistral_gpt2_perplexity_eval.sh
+++ bash/run_mistral_gpt2_perplexity_eval.sh
@@ -2,7 +2,6 @@
 #SBATCH --job-name=mini
 #SBATCH --array=0
 #SBATCH --time=2-12:00:00
-#SBATCH --gres=gpu:RTXA6000:2
 #SBATCH --ntasks=1
 #SBATCH --mem=120G
 #SBATCH --mail-type=ALL
@@ -20,4 +19,4 @@ cd /om/user/ehoseini/mistral/
 
 #python -m torch.distributed.launch --nproc_per_node=2 --nnodes=1 --node_rank=0 train.py --config conf/gpt2-mistral-small-config.yaml --training_arguments.fp16 true --training_arguments.per_device_train_batch_size 2 --model.initial_weights /om/user/ehoseini/MyData/mistral/caprica-gpt2-small-x81/ckpt_400000/pytorch_model.bin --run_training False
 
-python train.py --config conf/gpt2-mistral-small-config.yaml --training_arguments.fp16 true --training_arguments.per_device_train_batch_size 2 --model.initial_weights /om/user/ehoseini/MyData/mistral/caprica-gpt2-small-x81/ckpt_400000/pytorch_model.bin --run_training False
+CUDA_VISIBLE_DEVICES=0 python train.py --config conf/gpt2-mistral-small-config.yaml --training_arguments.fp16 true --training_arguments.per_device_train_batch_size 2 --model.initial_weights /om/user/ehoseini/MyData/mistral/caprica-gpt2-small-x81/ckpt_400000/pytorch_model.bin --run_training False
